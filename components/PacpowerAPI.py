9a6d9beacbed1478952cb7a88f31e61c79ffce93
adadesions <adadesions@gmail.com>
2020-05-31 02:56:01 +0700

    Complete version 1.0

---
+++ components/PacpowerAPI.py
@@ -0,0 +1,66 @@
+import json
+import growattServer
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+
+class PacpowerAPI:
+    def __init__(self, username, password, storage_id, maxVolt):
+        self.username = username
+        self.password = password
+        self.storage_id = storage_id
+        self.maxVolt = maxVolt
+        self.api = None
+        self.user_id = None
+        self.plant_id = None
+
+
+    def initAPI(self):
+        # Growatt Login
+        api = growattServer.GrowattApi()
+        login_response = api.login(self.username, self.password)
+
+        # Firebaes Login
+        cred = credentials.Certificate("fKey.json")
+        firebase_admin.initialize_app(cred, {'databaseURL': 'https://pacapi-ada92.firebaseio.com/'})
+
+        #  Assign to instance variable
+        self.api = api
+        self.user_id = login_response['userId']
+        plantInfo = self.api.plant_list(self.user_id)
+        self.plant_id = plantInfo['data'][0]['plantId']
+
+
+    def get_remain_battery(self):
+        raw_vbat = float(self.get_voltage())
+        percent = int((raw_vbat / self.maxVolt) * 100)
+
+        return percent
+
+
+    def get_voltage(self):
+        voltage = self.api.storage_detail(self.storage_id)['vbat']
+
+        return voltage
+
+
+    def get_data_pack(self):
+        package = {
+            'energyLevel': self.get_remain_battery(),
+            'voltage': self.get_voltage(),
+            'patternNo': 5,
+            'remainTime': '4 h 30 m',
+            'temp': '36 °C',
+            'kWatt': '250 kW-hr',
+            'ampere': '20,000 A',
+            'capacity': '35,000 mAh'
+        }
+
+        return package
+
+
+    def send_to_cloud(self, node_name):
+        ref = db.reference(node_name)
+        ref.push(self.get_data_pack())
+        print("=== Saved ===")
---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+import getopt
+import time
+from components.PacpowerAPI import PacpowerAPI
+
+
+if __name__ == '__main__':
+    opts, args = getopt.getopt(sys.argv[1:], 'u:p:s:v:l:',
+    ['user=', 'pass=', 'storage=', 'volt=', 'location='])
+
+    parsed_argv = {
+        'user': '', 'pass': '', 'storage_id': '', 'max_volt': '', 'location': ''
+    }
+
+    for key, val in opts:
+        print(key, val)
+        if key in ('-u', '--user'):
+            parsed_argv['user'] = val
+        if key in ('-p', '--pass'):
+            parsed_argv['pass'] = val
+        if key in ('-s', '--storage'):
+            parsed_argv['storage_id'] = val
+        if key in ('-v', '--volt'):
+            parsed_argv['max_volt'] = int(val)
+        if key in ('-l', '--location'):
+            parsed_argv['location'] = val
+    
+    print(parsed_argv)
+    # pAPI = PacpowerAPI('pacpower', 'pacpower1234', 'XJD59310BA', 53)
+    pAPI = PacpowerAPI(parsed_argv['user'], parsed_argv['pass'],
+    parsed_argv['storage_id'], parsed_argv['max_volt'])
+    pAPI.initAPI()
+
+    while True: 
+        newData = pAPI.get_data_pack()
+        print("NewData")
+        print(newData)
+        pAPI.send_to_cloud(parsed_argv['location'])
+
+        time.sleep(10)
